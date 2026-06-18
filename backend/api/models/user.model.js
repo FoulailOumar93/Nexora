169fc3277c2f4415c3c2b0fcc36d2d6c1609d81a
FoulailOumar93 <foulailoumar55@gmail.com>
2026-06-18 16:37:22 +0200

    Fixing MDP

--- backend/api/models/user.model.js
+++ backend/api/models/user.model.js
@@ -85,6 +85,18 @@ const User =
         type:
           DataTypes.STRING,
         allowNull: true
+      },
+
+      reset_password_token: {
+        type:
+          DataTypes.TEXT,
+        allowNull: true
+      },
+
+      reset_password_expires: {
+        type:
+          DataTypes.DATE,
+        allowNull: true
       }
     },
     {
